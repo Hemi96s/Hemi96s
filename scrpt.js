d890b7d1e491ea322ece33b0949b2a07bf4e9752
Hemi96s <h.fulva2020@gmail.com>
2025-02-04 17:09:37 +0800

    swiper

---
+++ scrpt.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: 'auto',
+
+  
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+
+  });
